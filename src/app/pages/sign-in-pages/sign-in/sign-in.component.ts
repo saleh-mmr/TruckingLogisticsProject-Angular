b9c10cbb57bb0bfde59e99d062cf3e02eef95140
alinadirkhanloo <alinadirkhanlo.98@gmail.com>
2022-04-06 12:22:33 +0430

    complete sign-in ui and flow

--- src/app/pages/sign-in-pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in-pages/sign-in/sign-in.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+  signIn(){
+    this.router.navigate(['sign-in/info']);
   }
 
 }
